695e4966efe2021133b80cd0cecab1a9755aeaf7
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-11-24 14:35:19 +0100

    dyn-terrain: Tidy the tessellation control shader, removing old experiments.

--- res/dyn-terrain/tess-control.glsl
+++ res/dyn-terrain/tess-control.glsl
@@ -23,15 +23,13 @@ void main() {
   // Pass through the position.
   gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 
-  // if (gl_InvocationID == 0) {
+  if (gl_InvocationID == 0) {
     float tessLevel = chooseTessLevel();
 
     gl_TessLevelInner[0] = tessLevel;
-    gl_TessLevelInner[1] = tessLevel;
 
     gl_TessLevelOuter[0] = tessLevel;
     gl_TessLevelOuter[1] = tessLevel;
     gl_TessLevelOuter[2] = tessLevel;
-    gl_TessLevelOuter[3] = tessLevel;
-  // }
+  }
 }
